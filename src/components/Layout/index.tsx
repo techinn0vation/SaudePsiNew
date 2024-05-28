a5984814248f546dd5647d0830a3ed04da783168
techinn0vation <techinnovation64@gmail.com>
2024-05-28 14:55:06 -0300

    Reajustando Componentes

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,9 +1,7 @@
 'use client'
 
-// import Script from 'next/script'
 import React, { ReactNode } from 'react'
 
-import Analytics from 'components/Analytics'
 import Analytics from 'components/Analytics'
 import Footer from 'components/Footer'
 import Menu from 'components/Menu'
@@ -25,20 +23,6 @@ export const Layout = ({ children }: LayoutProps) => {
     <StyledComponentsRegistry>
       <ThemeProvider theme={theme}>
         <html lang='pt-pt'>
-          {/* <head>
-            <Script
-              async
-              src='https://www.googletagmanager.com/gtag/js?id=G-L1S6MDPDRK'
-            />
-            <Script id='google-analytics'>
-              {`
- window.dataLayer = window.dataLayer || [];
- function gtag(){dataLayer.push(arguments);}
- gtag('js', new Date());
-
- gtag('config', 'G-L1S6MDPDRK');                `}
-            </Script>
-          </head> */}
           <body className={mainFont.className}>
             <Container>
               <Menu />
